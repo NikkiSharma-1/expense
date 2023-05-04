5510dd5408c6886421f0a2cb70bd8c09338303af
NikkiSharma-1 <114333988+NikkiSharma-1@users.noreply.github.com>
2023-05-04 13:42:41 +0530

    commit

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,47 +1,127 @@
 import React from 'react';
+import { useState,useRef } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 import classes from "./Login.module.css";
 
 function Login() {
-  return (
-    <section className={classes.auth}>
-      <h1>Sign Up</h1>
-      <form>
-        <div className={classes.control}>
-          <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="password">Your Password</label>
-          <input
-            type="password"
-            id="password"
-            required
-          />
-        </div>
+    const dispatch=useDispatch()
+    const history = useHistory();
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
+    const confirmPasswordInputRef = useRef();
+  
+    const [isLogin, setIsLogin] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+  
+    const switchAuthModeHandler = () => {
+      setIsLogin((prevState) => !prevState);
+    };
+    const passwordHandler=(props)=>{
+      history.replace('./passwdreset')
+  
+    }
+  
+    const SubmitHandler = (event) => {
+      event.preventDefault();
+  
+      const enteredEmail = emailInputRef.current.value;
+      const eneteredPassword = passwordInputRef.current.value;
+  
+      setIsLoading(true);
+      let url;
+  
+      if (isLogin) {
+        url =
+          "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDuvNa4o1FU9bVdjK2VHC-w9hXSFUJbsFo";
+      } else {
+        url =
+          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDuvNa4o1FU9bVdjK2VHC-w9hXSFUJbsFo";
+      }
+  
+      fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: eneteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => {
+          setIsLoading(false);
+          if (res.ok) {
+            return res.json();
+          } else {
+            return res.json().then((data) => {
+              let errorMessage = "Authentication Failed";
+              if (data && data.error && data.error.message) {
+                errorMessage = data.error.message;
+              }
+  
+              throw new Error(errorMessage);
+            });
+          }
+        })
+    };
+  
+    return (
+      <section className={classes.auth}>
+        <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+        <form onSubmit={SubmitHandler}>
           <div className={classes.control}>
-            <label htmlFor="password">Confirm Password</label>
+            <label htmlFor="email">Your Email</label>
+            <input type="email" id="email" required ref={emailInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="password">Your Password</label>
             <input
               type="password"
-              id="confirmPassword"
+              id="password"
+              required
+              ref={passwordInputRef}
             />
           </div>
-        <div className={classes.actions}>
-            <button>
-             Create Account<br />
+          {!isLogin && (
+            <div className={classes.control}>
+              <label htmlFor="password">Confirm Password</label>
+              <input
+                type="password"
+                id="confirmPassword"
+                // required
+                ref={confirmPasswordInputRef}
+              />
+            </div>
+          )}
+          <div className={classes.actions}>
+            {!isLoading && (
+              <button>
+                {isLogin ? "Login" : "Create Account"} <br />
+              </button>
+            )}
+            
+            {/* {isLogin && <button>Forgot Password</button>} */}
+            </div>
+            <div className={classes.actions}>
+  
+            <button
+              type="button"
+              className={classes.toggle}
+              onClick={switchAuthModeHandler}
+            >
+              {isLogin
+                ? "Don't have an account? Sign up!"
+                : "Have an account? Login"}
+              <br />
             </button>
           </div>
-          <div className={classes.actions}>
-          <button
-            type="button"
-            className={classes.toggle}
-          >
-            Already a User
-          </button>
-        </div>
-      </form>
-      <button className={classes.toggle}>Forgot password ?</button>
-    </section>
-  )
-}
-
-export default Login
+        </form>
+        {isLogin && <button className={classes.toggle} onClick={passwordHandler}>Forgot password ?</button>}
+  
+      </section>
+    );
+  };
+  
+  export default Login;

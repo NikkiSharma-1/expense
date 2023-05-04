699da505d0111003d7963eee2841093eb5fece8f
NikkiSharma-1 <114333988+NikkiSharma-1@users.noreply.github.com>
2023-05-04 14:04:53 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,69 @@
-import { Fragment } from "react";
+import { useEffect } from "react";
+import { Route,Switch } from "react-router-dom";
 import Login from "./Login/Login";
+import Welcome from "./Welcome/Welcome";
+import Profile from "./Profile/Profile";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { expenseActions } from "./Store/expense.slice";
+
+
 function App() {
+  const dispatch = useDispatch();
+  const theme = useSelector((state) => state.theme.darkMode);
+  const email = useSelector((state) => state.expense.email);
+  
+  
+
+  useEffect(() => {
+    if (!email) return;
+    console.log("before fetch", email);
+    fetch(
+      `https://expense-118bb-default-rtdb.firebaseio.com/${email}.json`,
+      {
+        method: "GET"
+      }
+    )
+      .then(async (res) => {
+        const data = await res.json();
+        console.log("In app js", data);
+        for (const key in data) {
+          const item = data[key];
+          item.id = key;
+          dispatch(expenseActions.addExpense(item));
+        }
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }, [dispatch, email]);
+
   return (
-   <Fragment>
-     <h1>Expense Tracker</h1>
-     <hr />
-     <Login />
-   </Fragment>
+    <main
+      style={
+        
+        theme
+          ? { backgroundColor: "black", color: "red" }
+          : { backgroundColor: "white" }
+      }
+    >
+      <Switch>
+        <Route path="/" exact>
+          <Login />
+        </Route>
+
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+
+        <Route path="/profile">
+          <Profile />
+        </Route>
+      </Switch>
+    </main>
   );
 }
 
